da364b002bb6a4967761265bc9d998f18bf9fd79
Brilianmadiuw <117911653+Brilianmadiuw@users.noreply.github.com>
2022-12-01 16:51:23 +0700

    Add files via upload

--- 1_D_71220931.py
+++ 1_D_71220931.py
@@ -3,34 +3,34 @@ print("Dimanakah kamu ingin menonton?")
 print("1) XXI Empire")
 print("1) XXI Amplaz")
 print("3) XXI JCM")
-def pilihanfilm():
-    a = input("masukan pilihanmu:")
-if a == 1 :
+
+a = input("masukan pilihanmu:")
+if a == "1" :
         print("mau nonton film apa nih? ada film:")
         print("1. Frozen")
         print("2. Keramat")
         print("3. KKN di Desa Penari")
 
 b = input("pilih nomor film:")
-if b == 2 :
+if b == "2" :
     print("1. Reguler")
     print("2. Dolby almos")
     print("3. Premiere")
 c = input("pilih nomor tipe layar:")
 d = input("berapa banyak tiket?")
-e = input("mau nonton jam berapa:")
+print("mau nonton jam berapa:")
 print("1. 12.35")
 print("2. 14.45")
 print("3. 16.55")
 print("4. 19.05")
 f = input("masukan pilihan anda:")
-if f == 1 :
+if f == "1" :
     print("oke berhasil!, silahkan dinikmati di jam 12.35")
-if f == 2 :
+if f == "2" :
     print("oke berhasil!, silahkan dinikmati di jam 14.45")
-if f == 3 :
+if f == "3" :
     print("oke berhasil!, silahkan dinikmati di jam 16.55")
-if f == 4 :
+if f == "4" :
     print("oke berhasil!, silahkan dinikmati di jam 19.05")
 
 
