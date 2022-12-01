5d6fd806b886ba75935da875834775c7ad75393f
Brilianmadiuw <117911653+Brilianmadiuw@users.noreply.github.com>
2022-12-01 16:10:45 +0700

    Add files via upload

---
+++ 1_D_71220931.py
@@ -0,0 +1,31 @@
+print("Hallo selamat datang di bioskop!")
+print("Dimanakah kamu ingin menonton?")
+print("1) XXI Empire")
+print("1) XXI Amplaz")
+print("3) XXI JCM")
+a = input("masukan pilihanmu:")
+if a == 1 :
+        print("mau nonton film apa nih? ada film:")
+        print("1. Frozen")
+        print("2. Keramat")
+        print("3. KKN di Desa Penari")
+
+b = input("pilih nomor film:")
+if b == 2 :
+    print("1. Reguler")
+    print("2. Dolby almos")
+    print("3. Premiere")
+c = input("pilih nomor tipe layar:")
+
+if c == 3 :
+    
+    
+
+
+
+
+   
+# def pilihanfilm():
+#     print("1. Frozen")
+#     print("2. Keramat")
+#     print("3.KKN di Desa Penari")
